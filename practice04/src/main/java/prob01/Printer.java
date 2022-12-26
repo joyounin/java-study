a8373793185280789dae48892249d6514c820af4
승학산-07 <-07@DESKTOP-SEMAJND>
2022-12-26 09:54:39 +0900

    Generic Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -1,24 +1,39 @@
 package prob01;
 
 public class Printer {
-	
-	public void println(int number) {
-		System.out.println(number);
-	}
 
-	public void println(boolean on) {
-		System.out.println(on);
-	}
+//	public void println(int number) {
+//		System.out.println(number);
+//	}
+//
+//	public void println(boolean on) {
+//		System.out.println(on);
+//	}
+//
+//	public void println(double dnumber) {
+//		System.out.println(dnumber);
+//	}
+//
+//	public void println(String name) {
+//		System.out.println(name);
+//		
+//	}
 
-	public void println(double dnumber) {
-		System.out.println(dnumber);
-	}
-
-	public void println(String name) {
-		System.out.println(name);
-		
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
+	public int sum(Integer... nums)  {	// 가변 파라미터 nums는 배열
+		int s = 0;
+		for(Integer n:nums) {
+			s += n;
+		}
+		return s;
+	}		
 	
-	
+	public <T> void println(T... ts) {
+		for(T t:ts) {
+		System.out.println(t);
+		}
+	}
 }
