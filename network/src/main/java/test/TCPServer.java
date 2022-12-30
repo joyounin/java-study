60619e59fc8d4442044b537f28f065a14c05c382
Joyounin <3723519@naver.com>
2022-12-30 14:41:50 +0900

    Socket Options #finisih

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -13,47 +13,56 @@ public class TCPServer {
 	public static void main(String[] args) {
 		ServerSocket serverSocket = null;
 		try {
-			// 1. Server Socket 생성
+			//1. Server Socket 생성
 			serverSocket = new ServerSocket();
 			
-			// 2. 바인딩(binding)
-			//	  Socket에 InetSocketaddress(IPAddress + Port)를 바인딩 한다.
-			//	  IPAddress: 0.0.0.0 : 특정 호스트 IP에 바인딩 하지 않는다.
-			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000), 10);		//10개는 클라이언트 가능수
+			//1-1. FIN_WAIT -> TIME_WAIT 상태에서도 소켓 포트 할당이 가능하도록 하기 위해
+			serverSocket.setReuseAddress(true);
 			
-			// 3. Accept
-			Socket socket = serverSocket.accept();	// bloking
+			//2. 바인딩(binding)
+			//   Socket에 InetSocketaddress(IPAddress + Port)를 바인딩 한다.
+			//   IPAddress: 0.0.0.0: 특정 호스트 IP에 바인딩 하지 않는다.
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000), 10);
+			
+			//3. accept
+			Socket socket = serverSocket.accept(); // blocking
 			
 			try {
-				// 포트랑 ip주소까지 포트는 TCP에 달려있다.
-				InetSocketAddress inetRemoteSocketAddress = 
-						(InetSocketAddress)socket.getRemoteSocketAddress();
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();
 				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-				int remoteport = inetRemoteSocketAddress.getPort();
-				System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remoteport + "]");
+				int remotePort = inetRemoteSocketAddress.getPort();
+				System.out.println("[server] connected by client[" + remoteHostAddress + ":" + remotePort + "]");
 				
-				// 4. IO Stream 받아오기
+				
+				//4. IO Stream 받아오기
 				OutputStream os = socket.getOutputStream();
-				InputStream is = socket.getInputStream();	
+				InputStream is = socket.getInputStream();
 				
 				while(true) {
-					// 5. 데이터 읽기
+					//5. 데이터 읽기
 					byte[] buffer = new byte[256];
-					int readByteCount = is.read(buffer); //blocking
+					int readByteCount = is.read(buffer); // blocking
 					if(readByteCount == -1) {
-						// 서버가 정상적으로 종료(close() 호출)
+						// 클라이언트가 정상적으로 종료(close() 호출)
 						System.out.println("[server] closed by client");
 						break;
 					}
 					
 					String data = new String(buffer, 0, readByteCount, "utf-8");
 					System.out.println("[server] received:" + data);
+
+					// SO_TIMEOUT 옵션 테스트용
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 					
-					// 6. 데이터 쓰기
+					//6. 데이터 쓰기					
 					os.write(data.getBytes("utf-8"));
 				}
 			} catch(SocketException ex) {
-				System.out.println("[server] suddnely closed by server:" + ex);
+				System.out.println("[server] suddenly closed by client");
 			} catch(IOException ex) {
 				System.out.println("[server] error:" + ex);
 			} finally {
